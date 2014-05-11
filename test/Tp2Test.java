82c6d2a4c62954a50719c1d67430c32011680d4b
zio12 <komah.ibrahime@hotmail.fr>
2014-05-11 00:21:53 -0400

    Added Aleatoire Unit Test and Tp2 empty test methods

---
+++ test/Tp2Test.java
@@ -0,0 +1,87 @@
+import static org.junit.Assert.fail;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class Tp2Test {
+	
+	private static Tp2 ojbectUnderTest;
+	
+	@BeforeClass
+	public static void setup() {
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		
+	}
+
+	@Test
+	public void testSupprimerSauvegarde() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSauvegarder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExistePartieSauvegardee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestaurer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLireOption() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLireSortePari() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLireMiseJoueur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfficherLesDeuxCartes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfficherFin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitialiserJeuDeCarte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoueurGagnant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMontantJoueur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPariFait() {
+		fail("Not yet implemented");
+	}
+
+}
